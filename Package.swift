b61989e407b9f1df38c235f355358b3f9c40bbd4
puchog <puchog@gmail.com>
2020-04-03 19:03:39 -0300

    Updated Package.swift to support SPM tools 5.2

--- Package.swift
+++ Package.swift
@@ -1,2 +1,18 @@
+// swift-tools-version:5.2
 import PackageDescription
-let package = Package(name: "Ion")
+let package = Package(
+    name: "Ion",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "Ion",
+            targets: ["Ion"]),
+    ],
+    targets: [
+        .target(
+            name: "Ion",
+            path: "Common")
+    ]
+)
