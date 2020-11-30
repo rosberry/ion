08bd659689d41fd30074a8ec6d9ae767fe5e45bb
zhvrnkov <vlad.zhavoronkov@rosberry.com>
2020-11-30 18:52:13 +0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,2 +1,15 @@
+// swift-tools-version:5.3
+
 import PackageDescription
-let package = Package(name: "Ion")
+
+let package = Package(
+        name: "Ion",
+        platforms: [.iOS(.v12)],
+        products: [
+            .library(name: "Ion", targets: ["Ion"])
+        ],
+        targets: [
+            .target(name: "Ion",
+                    path: "Common"),
+        ]
+)
