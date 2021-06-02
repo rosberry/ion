b6583f686410710da03a41d5ff02b042230a57ae
puchog <puchog@gmail.com>
2021-06-02 14:20:28 -0300

    Added SPM platforms

--- Package.swift
+++ Package.swift
@@ -2,9 +2,7 @@
 import PackageDescription
 let package = Package(
     name: "Ion",
-    platforms: [
-        .iOS(.v10)
-    ],
+    platforms: [.iOS(.v10), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v2)],
     products: [
         .library(
             name: "Ion",
